5ab8a5cf2812e104f594ef1567d70566ef5ebae6
Marky00000 <142792645+Marky00000@users.noreply.github.com>
2023-10-09 09:51:52 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+import React, { useState } from 'react';
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div>
+      <h1>Counter</h1>
+      <p>Current count: {count}</p>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
+}
+export default Counter;
+
+
